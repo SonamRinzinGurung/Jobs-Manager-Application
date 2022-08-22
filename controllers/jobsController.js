1eba41b693927ff6a6691dd0ea4ba83f2d7c6e8d
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-08-22 18:57:52 +0545

    Job Update and Delete server side

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from "../models/Job.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+import checkPermissions from "../utils/checkPermissions.js";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -21,11 +22,35 @@ const getAllJobs = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send("update job");
+  const { id: jobId } = req.params;
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const job = await Job.findOne({ _id: jobId });
+  if (!job) {
+    throw new NotFoundError(`No job with ${jobId} found.`);
+  }
+  checkPermissions(req.user, job.createdBy);
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  const { id: jobId } = req.params;
+  const job = await Job.findOne({ _id: jobId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId} found`);
+  }
+  checkPermissions(req.user, job.createdBy);
+  await job.remove();
+  res.status(StatusCodes.OK).json({ msg: "Job deleted" });
 };
 
 const showStats = async (req, res) => {
