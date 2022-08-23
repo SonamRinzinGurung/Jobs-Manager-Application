33debeffb4d9fe7f8b2b877fbdb39c2a970116ff
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-08-23 22:27:43 +0545

    jobs pagination backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -51,10 +51,19 @@ const getAllJobs = async (req, res) => {
   if (sort === "z-a") {
     result = result.sort("-position");
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+
+  const totalJobs = await Job.countDocuments(queryObj);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
