f1ff4cec4a4741e28301b18ce982866b74495728
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-08-23 11:53:26 +0545

    Monthly Application Bar Chart and Area Chart

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,5 +1,25 @@
-const AreaChart = () => {
-  return <div>AreaChart</div>;
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const AreaChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type="monotone" dataKey="count" stroke="#2cb1bc" fill="#bef8fd" />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 };
 
-export default AreaChart;
+export default AreaChartComponent;
