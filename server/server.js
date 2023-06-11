8c3e48667547015ba845e5142ae880e67388381e
sonam_rinzin <sonamrinzingurung@gmail.com>
2023-06-11 11:20:10 +0545

    HTML index directory changed

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use(express.json());
 app.use(helmet()); //secure headers
@@ -44,7 +44,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.use(notFoundMiddleware);
